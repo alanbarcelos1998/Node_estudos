056c9d0ecb292b0e666a33e11c79d7327b7a5f58
alanbarcelos1998 <alanbasiliobarcelos@hotmail.com>
2021-11-05 00:21:09 -0300

    Estrutura MVC, finalizacao dessa parte do estudo

--- 10_mvc/index.js
+++ 10_mvc/index.js
@@ -5,6 +5,10 @@ const app = express()
 
 const conn = require('./db/conn')
 
+const Task = require('./models/Task')
+
+const tasksRoutes = require('./routes/tasksRoutes')
+
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
 
@@ -18,4 +22,13 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
-app.listen(3000)
+app.use('/tasks', tasksRoutes)
+
+conn
+    .sync()
+    .then(() => {
+        app.listen(3000)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
